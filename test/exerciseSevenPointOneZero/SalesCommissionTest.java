361b175086fe0347d93da25e282889e2e1142715
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-12 17:40:20 +0100

    Sales commission program

---
+++ test/exerciseSevenPointOneZero/SalesCommissionTest.java
@@ -0,0 +1,38 @@
+package exerciseSevenPointOneZero;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalesCommissionTest {
+    SalesCommission salesCommission;
+
+    @BeforeEach
+    void setUp() {
+        salesCommission = new SalesCommission();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void basePayCanBeSetAndGotten(){
+        salesCommission.setBasePay(200);
+        assertEquals(200, salesCommission.getBasePay());
+    }
+
+    @Test
+    void grossSalesCanBeSetAndGotten(){
+        salesCommission.setGrossSales(5000);
+        assertEquals(5000, salesCommission.getGrossSales());
+
+    }
+
+    @Test
+    void salaryCanBeCalculated(){
+        assertEquals(650,  salesCommission.salaryCalculator(200, 5000));
+    }
+}

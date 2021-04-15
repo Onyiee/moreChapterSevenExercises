db679c0c8d15d467d5bfa96af288640290e63286
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-15 23:14:56 +0100

    test for dice rolling

--- test/exerciseSevenPointOneSeven/DiceRollingTest.java
+++ test/exerciseSevenPointOneSeven/DiceRollingTest.java
@@ -1,4 +1,45 @@
+package exerciseSevenPointOneSeven;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DiceRollingTest {
-  
+    DiceRolling diceRolling;
+    int[] arrayOfDiceResult = {0};
+
+
+    @BeforeEach
+    void setUp() {
+        diceRolling = new DiceRolling();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void sumOfRolledDiceCanBeGotten(){
+        diceRolling.rollBothDice();
+       assertNotEquals(0, diceRolling.getSumOfRolledDice());
+    }
+
+    @Test
+    void sumOfRolledDiceCanBeSavedInAnArray(){
+        int [] newArray = {9};
+        diceRolling.ArrayOfSumOfRolledDiceResult(9);
+//        for (int i = 0; i < arrayOfDiceResult.length; i++) {
+//            if (arrayOfDiceResult[i] == 0){
+//                arrayOfDiceResult[i] = 9;
+//            }
+//        }
+//        assertArrayEquals(newArray,diceRolling.getArrayOfSumOfRolledDiceResult());
+
+
+    }
 }

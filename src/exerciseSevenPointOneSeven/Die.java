8c70bb9e188691f54c0db79dfe5a21b8e464c111
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-15 23:13:14 +0100

    Die class

--- src/exerciseSevenPointOneSeven/Die.java
+++ src/exerciseSevenPointOneSeven/Die.java
@@ -1,2 +1,10 @@
-package exerciseSevenPointOneSeven;public class Die {
+package exerciseSevenPointOneSeven;
+
+import java.util.Random;
+
+public class Die {
+    private final Random random = new Random();
+    public int roll(){
+        return random.nextInt(6) + 1;
+    }
 }
